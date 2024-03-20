b92da5279b3b0cf86738f41edb2caa0cc909ada5
manuelkeck <72602863+manuelkeck@users.noreply.github.com>
2024-03-21 00:00:02 +0100

    Updated version number

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                   </main>
 
                   <footer className="bg-black text-center text-gray-500 py-4 px-6">
-                      <p className="text-white py-5">🚀 v1.64.3</p>
+                      <p className="text-white py-5">🚀 v1.65.0</p>
                       <p>&copy; Copyright 2024 Manuel Keck</p>
                   </footer>
 
