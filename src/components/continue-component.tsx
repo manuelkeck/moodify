97bc8d23dd7fe562e6a9411238dc0edb98c19b8b
manuelkeck <72602863+manuelkeck@users.noreply.github.com>
2024-10-25 13:59:26 +0200

    Removed beta patch for voice assistant

--- src/components/continue-component.tsx
+++ src/components/continue-component.tsx
@@ -29,10 +29,7 @@ const ContinueComponent = () => {
                         <Link href="/spotify-authorized/voice-assistant">
                             <div
                                 className="relative text-base px-2 py-3 mr-2 ml-2 mt-3 w-40 h-28 animate-border rounded-3xl bg-white bg-gradient-to-r from-red-500 via-purple-500 to-blue-500 bg-[length:400%_400%] flex items-center justify-center">
-                                <span
-                                    className="absolute w-14 rounded top-0.5 left-0.5 bg-red-600 text-white text-xs font-bold px-1 py-0.5 -rotate-[25deg] origin-top-left translate-x-[-20%] translate-y-[70%] bg-clip-border">
-                                    beta
-                                </span>
+
                                 Use Moodify as In-Car Voice Assistant
                             </div>
                         </Link>
